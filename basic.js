7e63ce2d77090b7750a0ac0fc60ce9137a0a77b1
SUBANU-KJY <harshinikjy@gmail.com>
2021-09-18 20:11:53 +0530

    1 CLASS

---
+++ basic.js
@@ -0,0 +1,30 @@
+//to print in JS
+console.log("SUBANU.K.J.Y");
+console.log(23);
+var message="WELCOME SUBANU!"
+console.log(message);
+
+//Declaration in JS
+    var a=5;
+    let b=11;
+    const c=3;
+console.log(a,b,c);
+a=1;
+b=2;
+console.log(a,b);
+//c=30;XXXX
+
+//OBJECTS
+// key:value
+const data={
+    name:"ANU",
+    age:17,
+    gender:"Female"
+}
+console.log(data);
+
+
+
+
+
+
