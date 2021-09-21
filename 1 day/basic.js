27fe757057745df2ad0dd3e449ac804a6afe7cbd
SUBANU-KJY <harshinikjy@gmail.com>
2021-09-21 20:46:11 +0530

    DAY 1

---
+++ 1 day/basic.js
@@ -0,0 +1,44 @@
+//to print in JS
+console.log("SUBANU.K.J.Y");
+console.log(23);
+var message="WELCOME SUBANU!"
+console.log(message);
+
+//Declaration in JS
+    var a=5;
+    let b=11;
+    const c=3;
+console.log(a,b,c);
+a=1;
+b=2;
+console.log(a,b);
+//c=30;XXXX
+
+//OBJECTS
+// key:value
+const data={
+    name:"ANU",
+    age:17,
+    gender:"Female"
+}
+console.log(data);
+console.log("SUBANU SAKTHI");
+
+//DATATYPES in JAVASCRIPT:
+/*
+--------------------------------------------------------------------------------------------------------
+Data          Types	Description	                                            Example
+String	   represents textual data	                              'hello', "hello world!" etc
+Number	   an integer or a floating-point number	               3, 3.234, 3e-2 etc.
+BigInt	   an integer with arbitrary precision	                   900719925124740999n , 1n etc.
+Boolean	   Any of two values: true or false	                       true and false
+undefined   a data type whose variable is not initialized	       let a;
+null	       denotes a null value	let a = null;
+Symbol	   data type whose instances are unique and immutable	   let value = Symbol('hello');
+Object	   key-value pairs of collection of data
+------------------------------------------------------------------------------------------------------
+*/
+
+
+
+
